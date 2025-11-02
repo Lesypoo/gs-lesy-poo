e823b2d7ec61c0d67ce77ec857505160a8ed786f
wafer7 <wafer7@CSIT345.myguest.virtualbox.org>
2025-11-02 18:57:14 -0500

    jenkins midterm :)

---
+++ src/main/java/com/example/leswork/LesworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.leswork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LesworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LesworkApplication.class, args);
+	}
+
+}
